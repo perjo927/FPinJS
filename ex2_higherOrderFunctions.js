27ab77c000165b6fb979a2325e8cda8fd8adba8c
Programmer Per <djpjgj@gmail.com>
2020-10-31 12:24:28 +0100

    Create ex2_higherOrderFunctions.js

---
+++ ex2_higherOrderFunctions.js
@@ -0,0 +1,6 @@
+//Higher-order function that accepts a func
+const higherOrderFunc1 = (func, x) => func(x);
+
+
+//Higher-order function that returns a func
+const higherOrderFunc2 = (x) => (y) => x + y; 
